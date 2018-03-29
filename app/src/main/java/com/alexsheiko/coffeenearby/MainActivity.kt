7f6cd4029bf3939f5f1707298ab26d2ea0bc703d
Alex Sheiko <alexsheikodev@gmail.com>
2018-03-29 20:08:23 +0300

    Add RecyclerView to layout

--- app/src/main/java/com/alexsheiko/coffeenearby/MainActivity.kt
+++ app/src/main/java/com/alexsheiko/coffeenearby/MainActivity.kt
@@ -3,11 +3,13 @@ package com.alexsheiko.coffeenearby
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.alexsheiko.coffeenearby.R.string
-import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.location.places.AutocompleteFilter.Builder
 import com.google.android.gms.location.places.AutocompleteFilter.TYPE_FILTER_ESTABLISHMENT
+import com.google.android.gms.location.places.AutocompletePredictionBufferResponse
 import com.google.android.gms.location.places.GeoDataClient
 import com.google.android.gms.location.places.PlaceDetectionClient
 import com.google.android.gms.location.places.Places
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val QUERY_STARBUCKS = "Starbucks"
@@ -17,21 +19,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mGeoDataClient: GeoDataClient
     private lateinit var mPlaceDetectionClient: PlaceDetectionClient
 
+    private val placesAdapter by lazy { PlacesAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Construct a GeoDataClient.
-        mGeoDataClient = Places.getGeoDataClient(this)
+        setupRecyclerView()
 
-        // Construct a PlaceDetectionClient.
-        mPlaceDetectionClient = Places.getPlaceDetectionClient(this)
+        initPlaceLoader()
 
-        val placeFilter = AutocompleteFilter.Builder().setTypeFilter(TYPE_FILTER_ESTABLISHMENT).build();
-        val results = mGeoDataClient.getAutocompletePredictions(QUERY_STARBUCKS, null,
-                placeFilter)
-
-        results.addOnCompleteListener { response ->
+        val request = createPlacesRequest()
+        request.addOnCompleteListener { response ->
             if (response.isSuccessful) {
                 // If places are loaded, show them in a list
                 response.result.forEach { item -> item.getPrimaryText(null) }
@@ -43,5 +42,26 @@ class MainActivity : AppCompatActivity() {
                 response.exception?.printStackTrace()
             }
         }
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView.apply {
+            setHasFixedSize(true)
+            adapter = placesAdapter
+        }
+    }
+
+    private fun initPlaceLoader() {
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this)
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this)
+    }
+
+    private fun createPlacesRequest(): Task<AutocompletePredictionBufferResponse> {
+        val placeFilter = Builder().setTypeFilter(TYPE_FILTER_ESTABLISHMENT).build()
+        return mGeoDataClient.getAutocompletePredictions(QUERY_STARBUCKS, null,
+                placeFilter)
     }
 }
