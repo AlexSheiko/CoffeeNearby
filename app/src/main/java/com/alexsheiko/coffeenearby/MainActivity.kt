8db3cc22528c2da6681dae0b0d537f594a979aa3
Alex Sheiko <alexsheikodev@gmail.com>
2018-03-30 17:03:04 +0300

    Request location updates instead of last known location

--- app/src/main/java/com/alexsheiko/coffeenearby/MainActivity.kt
+++ app/src/main/java/com/alexsheiko/coffeenearby/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.alexsheiko.coffeenearby
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import com.alexsheiko.coffeenearby.R.string
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.google.android.gms.location.places.*
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers.io
 import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
 private const val QUERY_STARBUCKS = "Starbucks"
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
     private lateinit var geoDataClient: GeoDataClient
@@ -30,13 +37,20 @@ class MainActivity : AppCompatActivity() {
 
         setupRecyclerView()
 
+        // Init user location provider.
+        initMyLocationProviderWithPermissionCheck()
+        // (The long method name above is caused by required "WithPermissionCheck" suffix
+        // added by permission dispatcher library. Long names are not the best coding style)
+
+        // Init nearby place loader that uses Google Places API
         initPlaceFinder()
-        initMyLocationProvider()
 
         // Add async operation to disposables to be able to stop and release resources onDestroy
         disposables.add(
-                // Load all places that match "Starbucks" query
-                loadPlaces(QUERY_STARBUCKS)
+                // Load last known user location without requesting location updates, to save battery
+                loadMyLocation()
+                        // Load all places that match "Starbucks" query
+                        .flatMap { myLocation -> loadPlacesNearby(myLocation, QUERY_STARBUCKS) }
                         // Convert response to a list of places
                         .flatMapIterable { response -> response.asIterable() }
                         // For each place, load additional info to show in a preview
@@ -48,11 +62,14 @@ class MainActivity : AppCompatActivity() {
                         // Get notified about the results on the main UI thread
                         .observeOn(mainThread())
                         // Afterwards, hide progress indicator
-                        .doAfterSuccess { statusTextView.text = null }
+                        .doAfterSuccess {
+                            statusTextView.text = null
+                            buttonRetry.visibility = GONE
+                        }
                         // Subscribe to observable chain to get results
                         .subscribe(
                                 {
-                                    // On success, show loaded place list in the recycler view
+                                    // On success, show loaded places in the recycler view
                                     placeList ->
                                     placesAdapter.addAll(placeList)
                                 },
@@ -65,19 +82,64 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onDestroy() {
+        // Stop observable chains and free up resources
         disposables.clear()
+        // Let Android system shutdown other activity services
         super.onDestroy()
     }
 
-    private fun handleError(it: Throwable) {
+    private fun handleError(error: Throwable) {
         // On error, notify user
         statusTextView.text = getString(string.error_loading_failed)
+        // Provide a hint about what the problem is, if available
+        hintTextView.text = error.message
+        // Enable Retry button
+        buttonRetry.visibility = VISIBLE
+        buttonRetry.setOnClickListener { restartApp() }
         // Log exception with more details
-        it.printStackTrace()
+        error.printStackTrace()
     }
 
-    private fun initMyLocationProvider() {
+    private fun restartApp() {
+        // Restart current activity
+        val intent = intent
+        finish()
+        startActivity(intent)
+        // Make transition smoother by removing screen re-opening animation
+        overridePendingTransition(0, 0)
+    }
+
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun initMyLocationProvider() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    // Permission request is handled in initMyLocationProvider(), suppressing the warning here
+    @SuppressLint("MissingPermission")
+    // Load last known user location to find coffee shops nearby
+    private fun loadMyLocation(): Observable<Location> {
+        return Observable.create { emitter ->
+            val locationRequest = LocationRequest.create().apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_LOW_POWER
+            }
+            val callback = object : LocationCallback() {
+                override fun onLocationResult(result: LocationResult?) {
+                    if (result != null) {
+                        // Publish current user location to query places
+                        val location = result.lastLocation
+                        emitter.onNext(location)
+                        // It's enough for this app to get a location update only once
+                        emitter.onComplete()
+                        fusedLocationClient.removeLocationUpdates(this)
+                    } else {
+                        emitter.onError(IllegalStateException("Current location is unknown"))
+                    }
+                }
+            }
+            fusedLocationClient.requestLocationUpdates(locationRequest, callback, null /* Looper */)
+        }
     }
 
     private fun setupRecyclerView() {
@@ -95,7 +157,8 @@ class MainActivity : AppCompatActivity() {
         placeDetectionClient = Places.getPlaceDetectionClient(this)
     }
 
-    private fun loadPlaces(query: String): Observable<AutocompletePredictionBufferResponse> {
+    private fun loadPlacesNearby(nearLocation: Location, query: String)
+            : Observable<AutocompletePredictionBufferResponse> {
         return Observable.create { emitter ->
             val request = geoDataClient.getAutocompletePredictions(query, null, null)
             request.addOnSuccessListener {
